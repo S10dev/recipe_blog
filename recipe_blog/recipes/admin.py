ae61b6adc6f1db7e96f436a3c1d721970332fbbb
Ioann <s10.dev@yandex.ru>
2021-10-01 03:05:16 +0300

    Added recipe models

--- recipe_blog/recipes/admin.py
+++ recipe_blog/recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recipe, Component, Component_quantity, Tag
 
 # Register your models here.
+
+admin.site.register(Recipe, )
+admin.site.register(Component, )
+admin.site.register(Component_quantity, )
+admin.site.register(Tag, )
--- recipe_blog/recipes/models.py
+++ recipe_blog/recipes/models.py
@@ -14,28 +14,51 @@ class Recipe(models.Model):
     picture = models.ImageField(blank=True, null=True)
     text = models.TextField(max_length=100, blank=True, null=True)
     components = models.ManyToManyField('Component')
-    tag = models.CharField()
+    tag = models.ManyToManyField('Tag')
     cooking_time = models.DateTimeField()
     slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         db_table = 'recipes'
 
 
 class Component(models.Model):
-    """Component structure from recipe"""
+    """Component structure"""
     title = models.CharField(max_length=50)
-    dimension = models.CharField(choices=('шт', 'кусок', 'г'))
+    dimension = models.CharField(
+        max_length=10,
+        choices=[
+            ('gram', 'г'),
+            ('piece', 'шт'),
+            ('ml', 'мл')
+            ]
+        )
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         db_table = 'components'
 
 
 class Component_quantity(models.Model):
-    """Table linking quantity and recipe with component together"""
+    """Table linking quantity with recipe and component together"""
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     compotent = models.ForeignKey(Component, on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
     class Meta:
         db_table = 'quantity_and_recipe_linking_table'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'tags_for_recipe'
